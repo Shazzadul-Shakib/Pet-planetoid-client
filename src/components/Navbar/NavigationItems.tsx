a6ec8460fe5e53906e724d0173d61731bcf13775
Shakib-Shazzadul <shakib1186@gmail.com>
2024-10-12 11:49:07 +0600

    activelink applied in navbar

--- src/components/Navbar/NavigationItems.tsx
+++ src/components/Navbar/NavigationItems.tsx
@@ -1,14 +1,18 @@
+"use client";
 import { NavIcons } from "@/icons/all-icons";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavigationItems: React.FC = () => {
+  const pathname = usePathname();
+
   return (
     <div className="mx-2 hidden items-center justify-end gap-8 text-xs md:flex lg:gap-14">
       {NavIcons.map((item) => (
         <Link
           href={item.path}
           key={item.title}
-          className="flex flex-col items-center gap-1 nav relative"
+          className={`${pathname === item.path ? "active" : ""} nav relative flex flex-col items-center gap-1`}
         >
           {item.icon}
           <span>{item.title}</span>
--- src/icons/all-icons.tsx
+++ src/icons/all-icons.tsx
@@ -3,7 +3,7 @@ import { Bell, House, MessageCircleMore, Store } from "lucide-react";
 
 export const NavIcons:TNavigation[] = [
   { icon: <House size={22}/>, title: "Home", path:"/" },
-  { icon: <Store size={22}/>, title: "Marketplace", path:"/" },
-  { icon: <MessageCircleMore size={22}/>, title: "Messages", path:"/" },
-  { icon: <Bell size={22}/>, title: "Notifications", path:"/" },
+  { icon: <Store size={22}/>, title: "Marketplace", path:"/marketplace" },
+  { icon: <MessageCircleMore size={22}/>, title: "Messages", path:"/messages" },
+  { icon: <Bell size={22}/>, title: "Notifications", path:"/notifications" },
 ];
