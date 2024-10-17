dbcb278a3ca26e5e7982e8d1cd4c596869f63991
Shakib-Shazzadul <shakib1186@gmail.com>
2024-10-17 11:18:14 +0600

    Image field in message added

--- src/components/messages/MessageCard.tsx
+++ src/components/messages/MessageCard.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+
 const MessageCard: React.FC = () => {
   return (
-    <div className="md: min-w-[200px] max-w-[300px] rounded-lg border border-secondary p-4">
-      <p>
+    <div className="max-w-[300px] rounded-lg border border-secondary p-4 md:min-w-[200px] md:max-w-[400px]">
+      {/* <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit
-      </p>
-      <p className="text-end text-xs">1m ago</p>
+      </p> */}
+      <div className="relative h-[150px] min-w-[200px] md:min-w-[300px]">
+        <Image
+          src="/portfolio.png"
+          fill
+          alt="Shared picture"
+          className=" object-contain"
+          quality={100}
+        />
+      </div>
+      <p className="text-end text-xs pt-2">1m ago</p>
     </div>
   );
 };
